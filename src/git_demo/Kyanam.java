66bdf6a35dfade1b1015c1a479159fabe2fa38f8
PremSai <pprem690@gmail.com>
2023-06-09 23:12:35 +0530

    2nd commit

--- src/git_demo/Kyanam.java
+++ src/git_demo/Kyanam.java
@@ -3,8 +3,10 @@ package git_demo;
 public class Kyanam {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hi");
+		
+		System.out.println(" hi");
+		System.out.println("PREM");
+		System.out.println(" SAI");
 		
 
 	}
